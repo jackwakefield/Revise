cb942c1f59501616a455b0c9977194e9f393f4a3
l3nn0x <dragon83.super@gmail.com>
2020-08-01 20:57:45 +0100

    Huge cleanup

---
+++ Revise/BoundingBox.cs
@@ -0,0 +1,356 @@
+﻿// Copyright (c) 2010-2014 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// -----------------------------------------------------------------------------
+// Original code from SlimMath project. http://code.google.com/p/slimmath/
+// Greetings to SlimDX Group. Original code published with the following license:
+// -----------------------------------------------------------------------------
+/*
+* Copyright (c) 2007-2011 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Numerics;
+
+namespace Revise
+{
+    /// <summary>
+    /// Represents an axis-aligned bounding box in three dimensional space.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct BoundingBox : IEquatable<BoundingBox>, IFormattable
+    {
+        /// <summary>
+        /// The minimum point of the box.
+        /// </summary>
+        public Vector3 Minimum;
+
+        /// <summary>
+        /// The maximum point of the box.
+        /// </summary>
+        public Vector3 Maximum;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoundingBox"/> struct.
+        /// </summary>
+        /// <param name="minimum">The minimum vertex of the bounding box.</param>
+        /// <param name="maximum">The maximum vertex of the bounding box.</param>
+        public BoundingBox(Vector3 minimum, Vector3 maximum)
+        {
+            this.Minimum = minimum;
+            this.Maximum = maximum;
+        }
+
+        /// <summary>
+        /// Returns the width of the bounding box
+        /// </summary>
+        public float Width
+        {
+            get { return this.Maximum.X - this.Minimum.X; }
+        }
+
+        /// <summary>
+        /// Returns the height of the bounding box
+        /// </summary>
+        public float Height
+        {
+            get { return this.Maximum.Y - this.Minimum.Y; }
+        }
+
+        /// <summary>
+        /// Returns the height of the bounding box
+        /// </summary>
+        public float Depth
+        {
+            get { return this.Maximum.Z - this.Minimum.Z; }
+        }
+
+        /// <summary>
+        /// Returns the size of the bounding box
+        /// </summary>
+        public Vector3 Size
+        {
+            get { return this.Maximum - this.Minimum; }
+        }
+
+        /// <summary>
+        /// Returns the size of the bounding box
+        /// </summary>
+        public Vector3 Center
+        {
+            get { return (this.Maximum + this.Minimum) * 0.5f; }
+        }
+
+        /// <summary>
+        /// Retrieves the eight corners of the bounding box.
+        /// </summary>
+        /// <returns>An array of points representing the eight corners of the bounding box.</returns>
+        public Vector3[] GetCorners()
+        {
+            Vector3[] results = new Vector3[8];
+            GetCorners(results);
+            return results;
+        }
+
+        /// <summary>
+        /// Retrieves the eight corners of the bounding box.
+        /// </summary>
+        /// <returns>An array of points representing the eight corners of the bounding box.</returns>
+        public void GetCorners(Vector3[] corners)
+        {
+            corners[0] = new Vector3(Minimum.X, Maximum.Y, Maximum.Z);
+            corners[1] = new Vector3(Maximum.X, Maximum.Y, Maximum.Z);
+            corners[2] = new Vector3(Maximum.X, Minimum.Y, Maximum.Z);
+            corners[3] = new Vector3(Minimum.X, Minimum.Y, Maximum.Z);
+            corners[4] = new Vector3(Minimum.X, Maximum.Y, Minimum.Z);
+            corners[5] = new Vector3(Maximum.X, Maximum.Y, Minimum.Z);
+            corners[6] = new Vector3(Maximum.X, Minimum.Y, Minimum.Z);
+            corners[7] = new Vector3(Minimum.X, Minimum.Y, Minimum.Z);
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="BoundingBox"/> that fully contains the given points.
+        /// </summary>
+        /// <param name="points">The points that will be contained by the box.</param>
+        /// <param name="result">When the method completes, contains the newly constructed bounding box.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="points"/> is <c>null</c>.</exception>
+        public static void FromPoints(Vector3[] points, out BoundingBox result)
+        {
+            if (points == null)
+                throw new ArgumentNullException("points");
+
+            Vector3 min = new Vector3(float.MaxValue);
+            Vector3 max = new Vector3(float.MinValue);
+
+            for (int i = 0; i < points.Length; ++i)
+            {
+                min = Vector3.Min(min, points[i]);
+                max = Vector3.Max(max, points[i]);
+            }
+
+            result = new BoundingBox(min, max);
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="BoundingBox"/> that fully contains the given points.
+        /// </summary>
+        /// <param name="points">The points that will be contained by the box.</param>
+        /// <returns>The newly constructed bounding box.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="points"/> is <c>null</c>.</exception>
+        public static BoundingBox FromPoints(Vector3[] points)
+        {
+            if (points == null)
+                throw new ArgumentNullException("points");
+
+            Vector3 min = new Vector3(float.MaxValue);
+            Vector3 max = new Vector3(float.MinValue);
+
+            for (int i = 0; i < points.Length; ++i)
+            {
+                min = Vector3.Min(min, points[i]);
+                max = Vector3.Max(max, points[i]);
+            }
+
+            return new BoundingBox(min, max);
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="BoundingBox"/> that is as large as the total combined area of the two specified boxes.
+        /// </summary>
+        /// <param name="value1">The first box to merge.</param>
+        /// <param name="value2">The second box to merge.</param>
+        /// <param name="result">When the method completes, contains the newly constructed bounding box.</param>
+        public static void Merge(ref BoundingBox value1, ref BoundingBox value2, out BoundingBox result)
+        {
+            result.Minimum = Vector3.Min(value1.Minimum, value2.Minimum);
+            result.Maximum = Vector3.Max(value1.Maximum, value2.Maximum);
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="BoundingBox"/> that is as large as the total combined area of the two specified boxes.
+        /// </summary>
+        /// <param name="value1">The first box to merge.</param>
+        /// <param name="value2">The second box to merge.</param>
+        /// <returns>The newly constructed bounding box.</returns>
+        public static BoundingBox Merge(BoundingBox value1, BoundingBox value2)
+        {
+            BoundingBox box;
+            box.Minimum = Vector3.Min(value1.Minimum, value2.Minimum);
+            box.Maximum = Vector3.Max(value1.Maximum, value2.Maximum);
+            return box;
+        }
+
+        /// <summary>
+        /// Tests for equality between two objects.
+        /// </summary>
+        /// <param name="left">The first value to compare.</param>
+        /// <param name="right">The second value to compare.</param>
+        /// <returns><c>true</c> if <paramref name="left"/> has the same value as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
+        [MethodImpl((MethodImplOptions)0x100)] // MethodImplOptions.AggressiveInlining
+        public static bool operator ==(BoundingBox left, BoundingBox right)
+        {
+            return left.Equals(ref right);
+        }
+
+        /// <summary>
+        /// Tests for inequality between two objects.
+        /// </summary>
+        /// <param name="left">The first value to compare.</param>
+        /// <param name="right">The second value to compare.</param>
+        /// <returns><c>true</c> if <paramref name="left"/> has a different value than <paramref name="right"/>; otherwise, <c>false</c>.</returns>
+        [MethodImpl((MethodImplOptions)0x100)] // MethodImplOptions.AggressiveInlining
+        public static bool operator !=(BoundingBox left, BoundingBox right)
+        {
+            return !left.Equals(ref right);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentCulture, "Minimum:{0} Maximum:{1}", Minimum.ToString(), Maximum.ToString());
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public string ToString(string format)
+        {
+            if (format == null)
+                return ToString();
+
+            return string.Format(CultureInfo.CurrentCulture, "Minimum:{0} Maximum:{1}", Minimum.ToString(format, CultureInfo.CurrentCulture),
+                Maximum.ToString(format, CultureInfo.CurrentCulture));
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <param name="formatProvider">The format provider.</param>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public string ToString(IFormatProvider formatProvider)
+        {
+            return string.Format(formatProvider, "Minimum:{0} Maximum:{1}", Minimum.ToString(), Maximum.ToString());
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="formatProvider">The format provider.</param>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (format == null)
+                return ToString(formatProvider);
+
+            return string.Format(formatProvider, "Minimum:{0} Maximum:{1}", Minimum.ToString(format, formatProvider),
+                Maximum.ToString(format, formatProvider));
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Minimum.GetHashCode() * 397) ^ Maximum.GetHashCode();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Vector4"/> is equal to this instance.
+        /// </summary>
+        /// <param name="value">The <see cref="Vector4"/> to compare with this instance.</param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="Vector4"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        [MethodImpl((MethodImplOptions)0x100)] // MethodImplOptions.AggressiveInlining
+        public bool Equals(ref BoundingBox value)
+        {
+            return Minimum == value.Minimum && Maximum == value.Maximum;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Vector4"/> is equal to this instance.
+        /// </summary>
+        /// <param name="value">The <see cref="Vector4"/> to compare with this instance.</param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="Vector4"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        [MethodImpl((MethodImplOptions)0x100)] // MethodImplOptions.AggressiveInlining
+        public bool Equals(BoundingBox value)
+        {
+            return Equals(ref value);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="value">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        /// <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object value)
+        {
+            if (!(value is BoundingBox))
+                return false;
+
+            var strongValue = (BoundingBox)value;
+            return Equals(ref strongValue);
+        }
+    }
+}
---
+++ Revise/ReaderWriter/BinaryWriter.cs
@@ -0,0 +1,233 @@
+﻿#region License
+
+/**
+ * Copyright (C) 2012 Jack Wakefield
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#endregion
+
+using System;
+using System.IO;
+using System.Text;
+using Revise;
+using System.Numerics;
+
+/// <summary>
+/// A collection of extensions for the <see cref="BinaryWriter"/> class.
+/// </summary>
+public static class BinaryWriterExtensions {
+    /// <summary>
+    /// The default encoding to be used when reading strings.
+    /// </summary>
+    public static Encoding DefaultEncoding;
+
+    /// <summary>
+    /// Initializes the <see cref="BinaryReaderExtensions"/> class.
+    /// </summary>
+    static BinaryWriterExtensions() {
+        DefaultEncoding = Encoding.GetEncoding("EUC-KR");
+    }
+
+    /// <summary>
+    /// Writes the specified string pre-fixed with the string length as a 8-bit integer to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    public static void WriteByteString(this BinaryWriter writer, string value) {
+        writer.WriteByteString(value, DefaultEncoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string pre-fixed with the string length as a 8-bit integer to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="encoding">The character encoding.</param>
+    public static void WriteByteString(this BinaryWriter writer, string value, Encoding encoding) {
+        writer.Write((byte)encoding.GetByteCount(value));
+        writer.WriteString(value, encoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string pre-fixed with the string length as a 16-bit integer to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    public static void WriteShortString(this BinaryWriter writer, string value) {
+        writer.WriteShortString(value, DefaultEncoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string pre-fixed with the string length as a 16-bit integer to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="encoding">The character encoding.</param>
+    public static void WriteShortString(this BinaryWriter writer, string value, Encoding encoding) {
+        writer.Write((short)encoding.GetByteCount(value));
+        writer.WriteString(value, encoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string pre-fixed with the string length as a 32-bit integer to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    public static void WriteIntString(this BinaryWriter writer, string value) {
+        writer.WriteIntString(value, DefaultEncoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string pre-fixed with the string length as a 32-bit integer to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="encoding">The character encoding.</param>
+    public static void WriteIntString(this BinaryWriter writer, string value, Encoding encoding) {
+        writer.Write(encoding.GetByteCount(value));
+        writer.WriteString(value, encoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    public static void WriteString(this BinaryWriter writer, string value) {
+        writer.WriteString(value, DefaultEncoding);
+    }
+
+    /// <summary>
+    /// Writes the specified string to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="encoding">The character encoding.</param>
+    public static void WriteString(this BinaryWriter writer, string value, Encoding encoding) {
+        writer.Write(encoding.GetBytes(value));
+    }
+
+    /// <summary>
+    /// Writes the specified string to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="length">The fixed length.</param>
+    public static void WriteString(this BinaryWriter writer, string value, int length, char paddingCharacter = '\0') {
+        writer.WriteString(value, length, DefaultEncoding, paddingCharacter);
+    }
+
+    /// <summary>
+    /// Writes the specified string to the underlying stream.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="length">The fixed length.</param>
+    /// <param name="encoding">The character encoding.</param>
+    public static void WriteString(this BinaryWriter writer, string value, int length, Encoding encoding, char paddingCharacter = '\0') {
+        byte[] values = encoding.GetBytes(value);
+        Array.Resize<byte>(ref values, length);
+
+        for (int i = encoding.GetByteCount(value); i < length; i++) {
+            values[i] = (byte)paddingCharacter;
+        }
+
+        writer.Write(values);
+    }
+
+    /// <summary>
+    /// Writes the specified matrix to the underlying stream.
+    /// </summary>
+    /// <param name="value">The matrix value.</param>
+    public static void Write(this BinaryWriter writer, Matrix4x4 value) {
+        writer.Write(value.M11);
+        writer.Write(value.M12);
+        writer.Write(value.M13);
+        writer.Write(value.M14);
+        writer.Write(value.M21);
+        writer.Write(value.M22);
+        writer.Write(value.M23);
+        writer.Write(value.M24);
+        writer.Write(value.M31);
+        writer.Write(value.M32);
+        writer.Write(value.M33);
+        writer.Write(value.M34);
+        writer.Write(value.M41);
+        writer.Write(value.M42);
+        writer.Write(value.M43);
+        writer.Write(value.M44);
+    }
+
+    /// <summary>
+    /// Writes the specified vector to the underlying stream.
+    /// </summary>
+    /// <param name="value">The vector value.</param>
+    public static void Write(this BinaryWriter writer, Vector2 value) {
+        writer.Write(value.X);
+        writer.Write(value.Y);
+    }
+
+    /// <summary>
+    /// Writes the specified vector to the underlying stream.
+    /// </summary>
+    /// <param name="value">The vector value.</param>
+    public static void Write(this BinaryWriter writer, Vector3 value) {
+        writer.Write(value.X);
+        writer.Write(value.Y);
+        writer.Write(value.Z);
+    }
+
+    /// <summary>
+    /// Writes the specified vector to the underlying stream.
+    /// </summary>
+    /// <param name="value">The vector value.</param>
+    public static void Write(this BinaryWriter writer, Vector4 value) {
+        writer.Write(value.X);
+        writer.Write(value.Y);
+        writer.Write(value.Z);
+        writer.Write(value.W);
+    }
+
+    /// <summary>
+    /// Writes the specified vector to the underlying stream.
+    /// </summary>
+    /// <param name="value">The vector value.</param>
+    public static void Write(this BinaryWriter writer, ShortVector3 value) {
+        writer.Write(value.X);
+        writer.Write(value.Y);
+        writer.Write(value.Z);
+    }
+
+    /// <summary>
+    /// Writes the specified vector to the underlying stream.
+    /// </summary>
+    /// <param name="value">The vector value.</param>
+    public static void Write(this BinaryWriter writer, ShortVector4 value) {
+        writer.Write(value.X);
+        writer.Write(value.Y);
+        writer.Write(value.Z);
+        writer.Write(value.W);
+    }
+
+    /// <summary>
+    /// Writes the specified vector to the underlying stream.
+    /// </summary>
+    /// <param name="value">The quaternion value.</param>
+    /// <param name="w">if set to <c>true</c> the value is written in the order WXYZ, else XYZW.</param>
+    public static void Write(this BinaryWriter writer, Quaternion value, bool w = false) {
+        if (w) {
+            writer.Write(value.W);
+        }
+
+        writer.Write(value.X);
+        writer.Write(value.Y);
+        writer.Write(value.Z);
+
+        if (!w) {
+            writer.Write(value.W);
+        }
+    }
+}
